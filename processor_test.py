d407f8890198ff939a67d53228817845f04ea3e0
Phillip Piper <phillip.piper@thetradedesk.com>
2018-10-08 08:04:53 +1100

    - Add unit test for "already processed" events

--- processor_test.py
+++ processor_test.py
@@ -1,52 +1,83 @@
 import unittest
 from mock import MagicMock
 
+from in_memory_redis import InMemoryRedis
 from processor import Processor
+
+CREATE_EVENT = {
+    "type": "event_callback",
+    "event": {
+        "event_ts": "1537991036.000100",
+        "type": "channel_created",
+        "channel": {
+            "name_normalized": "dev-test-1",
+            "name": "dev-test-1",
+            "creator": "U0BPCEYR4",
+            "created": 1537991036,
+            "id": "CD1USGKT7",
+        }
+    }
+}
+
+RENAME_EVENT = {
+    "type": "event_callback",
+    "event": {
+        "event_ts": "1537991036.000100",
+        "type": "channel_renamed",
+        "channel": {
+            "name_normalized": "dev-so-trial-notify",
+            "name": "dev-so-trial-notify",
+            "creator": "U0BPCEYR4",
+            "created": 1537991036,
+            "id": "CD1USGKT7",
+        }
+    }
+}
+
+CHANNEL_INFO_SUCCESS = {
+    'ok': True,
+    'channel': {
+        'created': 1533784859,
+        'creator': 'UAKA6GKFF',
+        'id': 'CC5D77M5Y',
+        'is_channel': True,
+        'members': ['UAKA6GKFF'],
+        'name': 'jpp-test-2',
+        'name_normalized': 'jpp-test-2',
+        'purpose': {
+            'creator': 'UAKA6GKFF',
+            'value': 'TESTING THIS'
+        },
+        'topic': {
+            'creator': u'UAKA6GKFF',
+            'value': 'Channel topic'
+        },
+    }
+}
+
+USER_INFO_SUCCESS = {
+    u'ok': True,
+    u'user': {
+        u'id': u'UAKA6GKFF',
+        u'name': u'phillip.piper',
+        u'profile': {
+            u'display_name': u'phillip.piper',
+            u'display_name_normalized': u'phillip.piper',
+            u'email': u'phillip.piper@thetradedesk.com',
+            u'first_name': u'Phillip',
+            u'image_24': u'https://avatars.slack-edge.com/2018-05-07/360275784695_b413a925836f89c22c8b_24.jpg',
+            u'last_name': u'Piper',
+            u'real_name': u'Phillip Piper',
+            u'real_name_normalized': u'Phillip Piper',
+            u'team': u'T0AT6LB9B',
+            u'title': u'Developer'
+        },
+        u'real_name': u'Phillip Piper'
+    }
+}
 
 
 class TestProcessor(unittest.TestCase):
-
-    CHANNEL_INFO_SUCCESS = {
-        'ok': True,
-        'channel': {
-            'created': 1533784859,
-            'creator': 'UAKA6GKFF',
-            'id': 'CC5D77M5Y',
-            'is_channel': True,
-            'members': ['UAKA6GKFF'],
-            'name': 'jpp-test-2',
-            'name_normalized': 'jpp-test-2',
-            'purpose': {
-                'creator': 'UAKA6GKFF',
-                'value': 'TESTING THIS'
-            },
-            'topic': {
-                'creator': u'UAKA6GKFF',
-                'value': 'Channel topic'
-            },
-        }
-    }
-
-    USER_INFO_SUCCESS = {
-        u'ok': True,
-        u'user': {
-            u'id': u'UAKA6GKFF',
-            u'name': u'phillip.piper',
-            u'profile': {
-                u'display_name': u'phillip.piper',
-                u'display_name_normalized': u'phillip.piper',
-                u'email': u'phillip.piper@thetradedesk.com',
-                u'first_name': u'Phillip',
-                u'image_24': u'https://avatars.slack-edge.com/2018-05-07/360275784695_b413a925836f89c22c8b_24.jpg',
-                u'last_name': u'Piper',
-                u'real_name': u'Phillip Piper',
-                u'real_name_normalized': u'Phillip Piper',
-                u'team': u'T0AT6LB9B',
-                u'title': u'Developer'
-            },
-            u'real_name': u'Phillip Piper'
-        }
-    }
 
     def test_create(self):
         expected_message = {
@@ -57,23 +88,10 @@ class TestProcessor(unittest.TestCase):
             'text': 'TESTING THIS',
             'title': '<#CC5D77M5Y>'
         }
-        event = {
-            "type": "event_callback",
-            "event": {
-                "event_ts": "1537991036.000100",
-                "type": "channel_created",
-                "channel": {
-                    "name_normalized": "dev-test-1",
-                    "name": "dev-test-1",
-                    "creator": "U0BPCEYR4",
-                    "created": 1537991036,
-                    "id": "CD1USGKT7",
-                }
-            }
-        }
+        event = CREATE_EVENT
         slack_client = MagicMock()
-        slack_client.user_info.return_value = self.USER_INFO_SUCCESS
-        slack_client.channel_info.return_value = self.CHANNEL_INFO_SUCCESS
+        slack_client.user_info.return_value = USER_INFO_SUCCESS
+        slack_client.channel_info.return_value = CHANNEL_INFO_SUCCESS
         logger = MagicMock()
         target_channel = "target"
 
@@ -98,23 +116,10 @@ class TestProcessor(unittest.TestCase):
             'text': 'TESTING THIS',
             'title': '<#CC5D77M5Y>'
         }
-        event = {
-            "type": "event_callback",
-            "event": {
-                "event_ts": "1537991036.000100",
-                "type": "channel_renamed",
-                "channel": {
-                    "name_normalized": "dev-so-trial-notify",
-                    "name": "dev-so-trial-notify",
-                    "creator": "U0BPCEYR4",
-                    "created": 1537991036,
-                    "id": "CD1USGKT7",
-                }
-            }
-        }
+        event = RENAME_EVENT
         slack_client = MagicMock()
-        slack_client.user_info.return_value = self.USER_INFO_SUCCESS
-        slack_client.channel_info.return_value = self.CHANNEL_INFO_SUCCESS
+        slack_client.user_info.return_value = USER_INFO_SUCCESS
+        slack_client.channel_info.return_value = CHANNEL_INFO_SUCCESS
         logger = MagicMock()
         target_channel = "target"
 
@@ -131,28 +136,35 @@ class TestProcessor(unittest.TestCase):
         self.assertDictEqual(posted_message, expected_message)
 
     def test_ignore_non_matching_names(self):
-        event = {
-            "type": "event_callback",
-            "event": {
-                "event_ts": "1537991036.000100",
-                "type": "channel_renamed",
-                "channel": {
-                    "name_normalized": "dev-so-trial-notify",
-                    "name": "dev-so-trial-notify",
-                    "creator": "U0BPCEYR4",
-                    "created": 1537991036,
-                    "id": "CD1USGKT7",
-                }
-            }
-        }
+        event = RENAME_EVENT
         slack_client = MagicMock()
         logger = MagicMock()
         processor = Processor("target", ["only-", "accept-", "these-"], slack_client, logger=logger)
         processor.process_channel_event("rename", event)
 
-        logger.info.assert_called_once_with("ignored... channel name doesn't start with the appropriate prefix: %s", "dev-so-trial-notify")
+        logger.info.assert_called_once_with("ignored... channel name doesn't start with the appropriate prefix: %s",
+                                            "dev-so-trial-notify")
         self.assertFalse(logger.error.called)
         self.assertFalse(slack_client.post_chat_message.called)
+
+    def test_ignore_second_event(self):
+        event = RENAME_EVENT
+        channel_id = event["event"]["channel"]["id"]
+        channel_name = event["event"]["channel"]["name"]
+
+        # Create a redis instance and tell it that it has already processed this channel
+        redis = InMemoryRedis()
+        redis.setnx("channel:%s" % channel_id, "already-seen")
+
+        slack_client = MagicMock()
+        logger = MagicMock()
+        processor = Processor("target", [], slack_client, redis_client=redis, logger=logger)
+        processor.process_channel_event("rename", event)
+
+        self.assertFalse(logger.error.called)
+        self.assertFalse(slack_client.post_chat_message.called)
+        logger.info.assert_called_once_with("ignored... we've already processed this channel: %s/%s", channel_id,
+                                            channel_name)
 
 
 if __name__ == '__main__':
